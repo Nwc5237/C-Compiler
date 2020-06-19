d8b02ddd5fdd848c1ce996ba7aacb4a612eb0ecd
NickCalabrese <nwc5237@psu.edu>
2020-06-19 18:04:33 -0400

    Getting everything ready for shunting yard
    Mostly just an upload so that I can work with the updated files from another computer.

--- parser.c
+++ parser.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lexer.h"
+#include "data_structures.h"
 #include "parser.h"
 
 int parse(char **pos)
 {
-    parse_expression(pos);
+    stack *operators=malloc(sizeof(stack)), *operands=malloc(sizeof(stack));
+    parse_expression(pos, operators, operands);
     return 1;
 }
 
-int parse_expression(char **pos)
+int parse_expression(char **pos, stack *operators, stack *operands)
 {
     char **lookahead = malloc(sizeof(char *));
     char **copy = malloc(sizeof(char *));
@@ -19,31 +21,28 @@ int parse_expression(char **pos)
     *lookahead = *pos;
     tok = malloc(sizeof(token));
 
-    if(!parse_factor(lookahead))
+    if(!parse_factor(lookahead, operators, operands))
         return 0;
 
     while(1)
     {
         *copy = *lookahead;
-        tok = scan(lookahead);
+        /*tok = scan(lookahead);
 
         if(!tok)
         {
             *lookahead = *copy;
             break;
         }
-
+*/
         //if the token is not a binary operator break
-        if(tok && strcmp(tok->token, "+") &&
-                  strcmp(tok->token, "-") &&
-                  strcmp(tok->token, "*") &&
-                  strcmp(tok->token, "/"))
+        if(!parse_binary_op(lookahead, operators, operands))
         {
             *lookahead = *copy;
             break;
         }
 
-        if(!parse_factor(lookahead))
+        if(!parse_factor(lookahead, operators, operands))
         {
             *lookahead = *copy;
             break;
@@ -54,7 +53,7 @@ int parse_expression(char **pos)
     return 1;
 }
 
-int parse_factor(char **pos)
+int parse_factor(char **pos, stack *operators, stack *operands)
 {
     char **lookahead = malloc(sizeof(char *));
     token *tok;
@@ -70,7 +69,7 @@ int parse_factor(char **pos)
 
         if(!strcmp(tok->token, "("))
         {
-            if(!parse_expression(lookahead))
+            if(!parse_expression(lookahead, operators, operands))
                 return 0;
 
             tok = scan(lookahead);
@@ -87,10 +86,39 @@ pass:
     return 1;
 }
 
-int parse_binary_op(char **pos)
+int parse_binary_op(char **pos, stack *operators, stack *operands)
 {
+    char **lookahead = malloc(sizeof(char *));
+    *lookahead = *pos;
+    token *tok;
+
+    tok = scan(lookahead);
+    if(tok &&
+            (!strcmp(tok->token, "+") ||
+             !strcmp(tok->token, "-") ||
+             !strcmp(tok->token, "*") ||
+             !strcmp(tok->token, "/")))
+    {
+        *pos = *lookahead;
+        return 1;
+    }
+
+    return 0;
+
 }
 
-int parse_unary_op(char **pos)
+int parse_unary_op(char **pos, stack *operators, stack *operands)
 {
+    char **lookahead = malloc(sizeof(char *));
+    *lookahead = *pos;
+    token *tok;
+
+    tok = scan(lookahead);
+    if(tok && !strcmp(tok->token, "-"))
+    {
+        *pos = *lookahead;
+        return 1;
+    }
+
+    return 0;
 }
--- parser.h
+++ parser.h
@@ -1,4 +1,6 @@
 #include "lexer.h"
+#include "data_structures.h"
+
 #ifndef EXPECT_H_
 #define EXPECT_H_
 void expect(char *expected, char **pos);
@@ -11,21 +13,21 @@ int parse(char **pos);
 
 #ifndef PARSE_EXPRESSION_H_
 #define PARSE_EXPRESSION_H_
-int parse_expression(char **pos);
+int parse_expression(char **pos, stack *operators, stack *operands);
 #endif // PARSE_EXPRESSION_H_
 
 #ifndef PARSE_FACTOR_H_
 #define PARSE_FACTOR_H_
-int parse_factor(char **pos);
+int parse_factor(char **pos, stack *operators, stack *operands);
 #endif // PARSE_FACTOR_H_
 
 #ifndef PARSE_BINARY_OP_H_
 #define PARSE_BINARY_OP_H_
-int parse_binary_op(char **pos);
+int parse_binary_op(char **pos, stack *operators, stack *operands);
 #endif // PARSE_BINARY_OP_H_
 
 #ifndef PARSE_UNARY_OP_H_
 #define PARSE_UNARY_OP_H_
-int parse_unary_op(char **pos);
+int parse_unary_op(char **pos, stack *operators, stack *operands);
 #endif // PARSE_UNARY_OP_H_
 
